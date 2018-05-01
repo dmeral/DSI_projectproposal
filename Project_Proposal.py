976454d63aef7e1ca0b8eb893c23e4e58d08936c
D. Meral <d.Mer.biophys@gmail.com>
2018-05-01 03:52:57 -0400

    Add files via upload

---
+++ Project_Proposal.py
@@ -0,0 +1,245 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from scipy import stats
+import pandas as pd
+from math import *
+import seaborn as sns
+from mpl_toolkits.basemap import Basemap
+
+
+data = pd.read_csv('NpdbPublicUseDataCsv/NPDB1710.CSV',delimiter=',',low_memory=False)
+
+
+state_pop = pd.read_excel('nst-est2016-01.xlsx',header=3,skip_footer=5)
+
+new_index_list = {}
+for s_label in state_pop.index:
+    try:
+        if s_label[0]=='.':
+            new_index_list.update({s_label: s_label[1:]})
+        else:
+            new_index_list.update({s_label: s_label})
+    except:
+        new_index_list.update({s_label: 'unknown'})
+
+state_pop.rename(new_index_list, axis='index',inplace=True)
+
+
+common_states = np.intersect1d(list(data["WORKSTAT"].unique()),list(state_pop["State Abb"]))
+
+
+data["PAYMENT"].replace('[\$,]', '', regex=True, inplace=True)
+
+
+pay_count = None
+for state in common_states:
+    #print(state)
+    arrays = [[state,state], ['pay_per_case', 'count_per_1000000']]
+    column_names = pd.MultiIndex.from_arrays(arrays).T
+    mean_payment = [pd.to_numeric(data[(data["WORKSTAT"]==state) & (data["ORIGYEAR"]==i)]['PAYMENT']).mean() for i in range(1990,2018)]
+    count_per_capita = [1000000*len(data[(data["WORKSTAT"]==state) & (data["ORIGYEAR"]==i)])/state_pop[state_pop["State Abb"]==state][2016][0] for i in range(1990,2018)]
+    stack = pd.DataFrame(np.stack((mean_payment,
+                                   count_per_capita),axis=0).T,
+                         columns=column_names,
+                         index=range(1990,2018))
+    #print(stack)
+    if type(pay_count)==None:
+        pay_count = stack
+    else:
+        pay_count = pd.concat([pay_count, stack], axis=1)
+
+
+
+state_slope = []
+for state in common_states:
+    slope, intercept, rvalue, pvalue, stderr = scipy.stats.linregress(range(1991,2018),
+                                                                      pay_count[state,"count_per_1000000"][1:])
+    state_slope.append([state,slope,intercept])
+    
+df_state_slope=pd.DataFrame(state_slope,columns=["state","slope","intercept"])
+#df_state_slope #.sort_values("slope")
+
+
+correlation_slope = np.zeros((len(common_states),len(common_states)))
+
+for state1 in range(len(common_states)):
+    for state2 in range(len(common_states)):
+        #print(float(df_state_slope[df_state_slope["state"]==common_states[state1]]["slope"]))
+        #print(float(df_state_slope[df_state_slope["state"]==common_states[state2]]["slope"]))
+        correlation_slope[state1,state2] = ((
+            float(df_state_slope[df_state_slope["state"]==common_states[state1]]["slope"])-
+            float(df_state_slope[df_state_slope["state"]==common_states[state2]]["slope"])))**2
+        
+
+from sklearn.cluster import DBSCAN
+# Compute DBSCAN
+
+db = DBSCAN(eps=0.05, min_samples=1,metric='precomputed').fit_predict(correlation_slope)
+
+df_state_cluster = pd.DataFrame(db.T,columns=["cluster"])
+df_state_slope_cluster = pd.concat([df_state_slope,df_state_cluster],axis=1)
+
+average_slopes = []
+for state in common_states:
+    current_cluster = int(df_state_slope_cluster[df_state_slope_cluster["state"]==state]["cluster"])
+    #print(current_cluster)
+    average_slope = df_state_slope_cluster[df_state_slope_cluster["cluster"]==current_cluster]["slope"].mean()
+    average_slopes.append(average_slope)
+    
+df_cluster_slope = pd.DataFrame(np.array(average_slopes).T,columns=["avg_slopes"])
+df_state_slope_cluster_aslope = pd.concat([df_state_slope_cluster,df_cluster_slope],axis=1)
+
+cluster_colors = pd.DataFrame(np.array(average_slopes).T,columns=["cl_colors"])
+df_state_slope_cluster_aslope_acolor = pd.concat([df_state_slope_cluster_aslope,cluster_colors],axis=1)
+
+start = 0
+for i in np.unique(df_state_slope_cluster_aslope["avg_slopes"].sort_values()):
+    #print(i)
+    #print(start)
+    df_state_slope_cluster_aslope_acolor.replace(to_replace= {'cl_colors': {i: start}}, inplace=True)
+    start += 1
+
+
+from bokeh.io import show, save, output_notebook, export_png
+from bokeh.models import (
+    ColumnDataSource,
+    HoverTool,
+    ContinuousColorMapper,
+    LabelSet,
+    Label,
+    LogColorMapper, 
+    LogTicker, 
+    FixedTicker,
+    ColorBar, 
+    LinearColorMapper,
+    Legend
+)
+from bokeh.plotting import figure, output_file, show
+from bokeh.sampledata.us_states import data as states
+from bokeh.resources import CDN
+from bokeh.embed import file_html
+from bokeh.palettes import PRGn11 as palette
+from bokeh.palettes import Category20,Spectral11,Category10,PRGn11
+
+
+
+try:
+    del states["HI"]
+    del states["AK"]
+except:
+    pass
+
+palette.reverse()
+
+states = {
+    code: state for code, state in states.items()
+}
+#print(states)
+
+state_xs = [states[code]["lons"] for code in states]
+state_ys = [states[code]["lats"] for code in states]
+state_names = [state['name'] for state in states.values()]
+
+state_rates = []
+for name in state_names:
+    abbr = state_pop.loc[name]["State Abb"]
+    state_rates.append(float(df_state_slope[df_state_slope["state"]==abbr]["slope"]))
+#print(state_rates)
+
+lat_inkm = 111.132 ## at around lat = 45degrees from the wiki latitude page
+lon_inkm = 78.847 ## at around lat = 45degrees from the wiki latitude page
+
+
+
+text_position_x = []
+for statex in state_xs:
+    text_position_x.append(float("%.5f" % round(max(statex)-(max(statex)-min(statex))/2,5)))
+    
+text_position_y = []
+for statey in state_ys:
+    text_position_y.append(float("%.5f" % round(max(statey)-(max(statey)-min(statey))/2,5)))
+
+
+
+state_clusters=[]
+for name in state_names:
+    abbr = state_pop.loc[name]["State Abb"]
+    state_clusters.append(palette[int(df_state_slope_cluster_aslope_acolor[df_state_slope_cluster_aslope_acolor["state"]==abbr]["cl_colors"])])
+
+#state_clusters
+
+
+
+cluster_label = []
+for name in state_names:
+    abbr = state_pop.loc[name]["State Abb"]
+    cluster_label.append(int(df_state_slope_cluster_aslope_acolor[df_state_slope_cluster_aslope_acolor["state"]==abbr]["cluster"]))
+
+#cluster_label
+
+
+output_notebook()
+
+color_mapper = LinearColorMapper(palette=palette, low=min(state_rates), high=max(state_rates))
+
+source = ColumnDataSource(data=dict(
+    x=state_xs,
+    y=state_ys,
+    name=state_names,
+    rate=state_rates,
+    clusters=state_clusters,
+    cluster_label=cluster_label,
+))
+
+TOOLS = "pan,wheel_zoom,reset,hover,save"
+
+p = figure(title="Change in the number of malpractice cases filed per a million citizens per states, 1990-2018", 
+    plot_width=int((max(max(state_xs))-min(min(state_xs)))*lon_inkm/4.5), 
+    plot_height=int((max(max(state_ys))-min(min(state_ys)))*lat_inkm/4.5), tools=TOOLS,
+    x_axis_location=None, y_axis_location=None
+)
+
+p.grid.grid_line_color = None
+
+p.patches('x', 'y', source=source,
+          fill_color='clusters',
+          fill_alpha=0.7, line_color="gray", line_width=0.5)
+
+color_bar = ColorBar(color_mapper=color_mapper, #ticker=ticker,
+                     label_standoff=12, border_line_color=None, location=(0,0))
+
+p.add_layout(color_bar, 'right')
+
+hover = p.select_one(HoverTool)
+hover.point_policy = "follow_mouse"
+hover.tooltips ="""
+    <font size="3">State: <strong>@name</strong> </font> <br>
+    <font size="3">Change in the number of malpractice cases </font> <br>
+    <font size="3">per year from 1990 to 2018: <strong>@rate per 1,000,000</strong> </font> <br>
+    <font size="3">Cluster: <strong>@cluster_label</strong> </font>
+"""
+
+show(p)
+
+
+
+p = figure(plot_width=850, plot_height=500, tools=[hover], x_axis_label='Years', y_axis_label='Malpractice cases filed per 1,000,000')
+p.title.text = "Malpractice case per 1,000,000 citizens per year for a group of states which successfully diminished this number in the last two decades."
+
+cluster_num = 6
+cur_states = list(df_state_slope_cluster_aslope_acolor[df_state_slope_cluster_aslope_acolor["cluster"]==cluster_num]["state"])
+
+for state, color in zip(cur_states, Category20[20]):
+    curve = pay_count[state,"count_per_10000"]
+    plt.plot(range(1990,2018),curve,label=state,marker='.')
+    p.line(curve.index, curve,line_width=2, color=color, alpha=1.,
+           muted_color=color, muted_alpha=0.2, legend=state)
+
+p.legend.location = "top_left"
+p.legend.click_policy="mute"
+
+output_notebook()
+
+show(p)
+
